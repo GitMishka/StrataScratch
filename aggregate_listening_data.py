0c18ceca05bba748b418d696b1edc56111ade4b3
Misha <mishagrigoryan@gmail.com>
2024-03-12 23:38:59 -0400

    spotify

---
+++ aggregate_listening_data.py
@@ -0,0 +1,12 @@
+# Import your libraries
+import pandas as pd
+
+listening_habits['listen_duration'].fillna(0, inplace=True)
+
+grouped_data = listening_habits.groupby('user_id').agg(
+    total_listen_duration=('listen_duration', 'sum'),
+    unique_song_count=('song_id','nunique')).reset_index()
+
+grouped_data['total_listen_duration'] = (grouped_data['total_listen_duration'] / 60).round()
+
+grouped_data
