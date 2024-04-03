19807b8015cdb582b26cd451ade40c90d3f8f149
Misha <mishagrigoryan@gmail.com>
2024-04-03 00:02:27 -0400

    fixed

--- sql_easy/rank.sql
+++ sql_easy/rank.sql
@@ -1,18 +1,15 @@
 with ranks as (select  
-rank() over (partition by product_category order by total_sales desc) as rnk
-, product_category
-, market_place
-, total_sales
-, seller_id
-, month 
-from sales_data)
-#select * from ranks
+*,
+DENSE_RANK() over (partition by product_category order by total_sales desc) as rnk
+from sales_data where month = '2024-01') 
+
+
 SELECT
     product_category,
     seller_id,
     total_sales,
     month
 FROM ranks
-where month = '2024-01' and rnk <=3
+where rnk <=3
 ORDER BY
     product_category, rnk;
