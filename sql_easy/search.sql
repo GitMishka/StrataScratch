3f1eeaf8c3000f6889ed41a484b51a9d5ddd9c6c
Misha <mishagrigoryan@gmail.com>
2024-04-06 21:32:13 -0400

    3

---
+++ sql_easy/search.sql
@@ -0,0 +1,1 @@
+select count(*) from airbnb_search_details where (property_type = 'House') and (amenities like '%TV%') and (neighbourhood = 'Westlake')
---
+++ sql_easy/user1.sql
@@ -0,0 +1,1 @@
+select count(*) from facebook_friends where user1 not in (1) and user2 not in (1)
