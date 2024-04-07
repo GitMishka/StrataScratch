913698917e9b3932a97241658e34360db1de1552
Misha <mishagrigoryan@gmail.com>
2024-04-06 21:46:45 -0400

    moves

--- 1/yelp.py
+++
@@ -1,8 +0,0 @@
-# Import your libraries
-import pandas as pd
-
-# Start writing code
-yelp_business.head()
-
-df = yelp_business[yelp_business['categories'].str.contains('pizza', case = False)]
-df['categories'].count()
