9f248ec841471a9b3dd5831e1f5fea5062c0b64f
Misha <mishagrigoryan@gmail.com>
2024-04-09 23:56:15 -0400

    1

--- sql_medium/emails.sql
+++ sql_medium/emails.sql
@@ -4,3 +4,14 @@ SELECT from_user,
 FROM google_gmail_emails
 GROUP BY from_user
 ORDER BY total_emails DESC, from_user ASC
+
+/ 
+
+
+with cte1 as (select from_user, count(*) as counts from google_gmail_emails
+group by from_user ) 
+
+select 
+ * ,
+ row_number() over (order by counts desc, from_user ASC)
+ from cte1
