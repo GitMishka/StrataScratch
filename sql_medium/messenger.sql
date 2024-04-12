4e91f55c2cac78b8d0e2d379051ab150b3c4599e
Misha <mishagrigoryan@gmail.com>
2024-04-12 00:14:28 -0400

    3

--- sql_medium/messenger.sql
+++ sql_medium/messenger.sql
@@ -17,3 +17,27 @@ SELECT username, total_msg_count
 FROM sq
 WHERE rank <= 10
 ORDER BY total_msg_count DESC;
+
+WITH SentMessages AS (
+    SELECT user1 AS username, SUM(msg_count) AS sent_count
+    FROM fb_messages
+    GROUP BY user1
+),
+ReceivedMessages AS (
+    SELECT user2 AS username, SUM(msg_count) AS received_count
+    FROM fb_messages
+    GROUP BY user2
+),
+CombinedCounts AS (
+    SELECT
+        ISNULL(s.username, r.username) AS username, 
+        ISNULL(s.sent_count, 0) AS sent_count,
+        ISNULL(r.received_count, 0) AS received_count
+    FROM SentMessages s
+    FULL OUTER JOIN ReceivedMessages r ON s.username = r.username
+)
+SELECT TOP 10
+    username,
+    (sent_count + received_count) AS total_messages
+FROM CombinedCounts
+ORDER BY total_messages DESC;
