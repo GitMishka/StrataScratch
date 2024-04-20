39e3c7be72c71d4c546a2f0177854b0e1c4939f5
Misha <mishagrigoryan@gmail.com>
2024-04-20 00:00:18 -0400

    1

---
+++ python_easy2/feb.py
@@ -0,0 +1,6 @@
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+worker.head()
+feb = worker[(worker['joining_date'] >= '2014-02-01') & (worker['joining_date'] <= '2014-02-28')]
