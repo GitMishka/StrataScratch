ef627a8a28b8fadebd407f8bffaf3e594e6a4a98
Misha <mishagrigoryan@gmail.com>
2024-07-24 20:37:45 -0400

    2

--- sql_easy2/loans.sql
+++ sql_easy2/loans.sql
@@ -4,3 +4,10 @@ JOIN loans b
 ON a.user_id = b.user_id
 WHERE a.type = 'Refinance'
 AND b.type = 'InSchool';
+
+
+SELECT user_id
+FROM loans
+WHERE type IN ('Refinance', 'InSchool')
+GROUP BY user_id
+HAVING COUNT(DISTINCT type) = 2;
