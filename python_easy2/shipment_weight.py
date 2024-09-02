75049fc6848622a106d9ef4fe770049b2c19861a
Misha <mishagrigoryan@gmail.com>
2024-09-01 23:18:21 -0400

    3

--- python_easy2/shipment_weight.py
+++ python_easy2/shipment_weight.py
@@ -4,3 +4,5 @@ import pandas as pd
 # Start writing code
 amazon_shipment.head()
 df = (amazon_shipment.groupby('shipment_id')['weight'].sum().to_frame('total_weight').reset_index())
+df['rank'] = df['total_weight'].rank(method='dense', ascending=False)
+result = df[['shipment_id','total_weight']][df['rank'] == 3]
