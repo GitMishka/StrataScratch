bb503334fe047f9a79e1db7624329d9de6f96ba7
Misha <mishagrigoryan@gmail.com>
2024-09-01 23:31:07 -0400

    2

--- python_easy2/idmax.py
+++ python_easy2/idmax.py
@@ -7,3 +7,20 @@ data = pd.Series([1, 3, 7, 2, 5])
 index_of_max = data.idxmax()
 
 print(index_of_max)
+import pandas as pd
+
+# Create a Pandas DataFrame
+data = pd.DataFrame({
+    'worker_id': [1, 2, 3, 4, 5],
+    'login_timestamp': pd.to_datetime([
+        '2023-08-01 08:00:00', 
+        '2023-08-01 09:00:00', 
+        '2023-08-01 10:00:00', 
+        '2023-08-01 11:00:00', 
+        '2023-08-01 12:00:00'])
+})
+
+# Suppose we want the row index of the latest login
+index_of_latest_login = data['login_timestamp'].idxmax()
+
+print(index_of_latest_login)  # Output: 4 (because the last timestamp is the maximum)
