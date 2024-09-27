38e6f1ee1f6b2e7ed33140d60427ef0441428b68
Misha <mishagrigoryan@gmail.com>
2024-09-26 23:58:19 -0400

    1

---
+++ python_easy3/flightmovie.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+flight_movies_cross = pd.merge(flight_schedule.assign(key=1), entertainment_catalog.assign(key=1), on='key').drop('key', axis=1)
+
+suitable_movies = flight_movies_cross[flight_movies_cross['duration'] <= flight_movies_cross['flight_duration']]
+
+suitable_movies_sorted = suitable_movies.sort_values(by=['flight_id', 'duration'], ascending=[True, True])
+
+result = suitable_movies_sorted[['flight_id', 'movie_id', 'duration']]
+result.rename(columns={'duration': 'movie_duration'}, inplace=True)
+
+result[result['flight_id'] == 101]
